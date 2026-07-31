19a1b844bffa0c8d26b325025f99bdefc23f06f5
bhawna <bhawnanautiyalll@gmail.com>
2026-07-31 18:11:28 +0530

    Improve code readability

--- backend/src/services/syncInventoryToRedis.js
+++ backend/src/services/syncInventoryToRedis.js
@@ -1,55 +1,18 @@
 const db = require("../config/firebaseAdmin");
 
-const {
-    redisClient
-} = require("../config/redisClient");
+const {redisClient} = require("../config/redisClient");
 
-const syncInventoryToRedis =
-async () => {
-
+const syncInventoryToRedis = async () => {
     try {
-
-        const snapshot =
-        await db
-        .collection("products")
-        .get();
-
-        for (
-            const doc
-            of snapshot.docs
-        ) {
-
-            const product =
-            doc.data();
-
-            const key =
-            `product:${doc.id}:stock`;
-
-            await redisClient.set(
-                key,
-                product.stock
-            );
-
-            console.log(
-                `Synced ${key} = ${product.stock}`
-            );
-
+        const snapshot =await db.collection("products").get();
+        for (const doc of snapshot.docs) {
+            const product =doc.data();
+            const key = `product:${doc.id}:stock`;
+            await redisClient.set(key, product.stock);
         }
-
-        console.log(
-            "Inventory Sync Complete"
-        );
-
     }
     catch(error){
-
-        console.log(
-            "Redis Sync Error:",
-            error
-        );
-
     }
-
 };
 
 module.exports = {
